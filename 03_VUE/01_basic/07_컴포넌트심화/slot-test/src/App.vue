eca29c4230336b45d370bba08bbd789d9b345511
bigwaveBigwave <bluebiird909@naver.com>
2026-03-29 04:06:01 +0900

    14

---
+++ 03_VUE/01_basic/07_컴포넌트심화/slot-test/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NoSlotTest from './components/example1/NoSlotTest.vue';
+import SlotTest from './components/example1/SlotTest.vue';
+
+NoSlotTest;
+</script>
+
+<template>
+  <!--<NoSlotTest></NoSlotTest>-->
+  <SlotTest/>
+</template>
+
+<style scoped></style>
